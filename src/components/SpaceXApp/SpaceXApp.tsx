534478e35215183a019cf77a6e0d13fb5cfb12b8
Muhammad Umair <hafizumairtariq1234@gmail.com>
2021-06-27 17:09:22 +0500

    updated

--- src/components/SpaceXApp/SpaceXApp.tsx
+++ src/components/SpaceXApp/SpaceXApp.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import {Jumbotron} from 'react-bootstrap';
+import {
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+    }
+  }
+`;
+
+function ExchangeRates() {
+
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.rates.map(({ currency, rate }:any) => (
+    <div key={currency}>
+      <p>
+        {currency}: {rate}
+      </p>
+    </div>
+  ));
+}
 
 function SpaceXApp() {
   return (
     <div>
       <Jumbotron>SpaceX Client App</Jumbotron>
+      <ExchangeRates />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SpaceXApp from './components/SpaceXApp/SpaceXApp';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <SpaceXApp />
+      <ApolloProvider client={client}>
+        <SpaceXApp />
+      </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
